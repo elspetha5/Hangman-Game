13e74a64e2baae3df6657563c5e18baea3e2bcfe
Elspeth Anderson <elspetha@gmail.com>
2018-02-14 18:10:27 -0700

    update HTML

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+// Way to count wins
+// Way to insert current word
+// Way to change each blank space into a the letter it's supposed to be
+// Count down number of guesses and reset with new words
+// Insert letters already guessed in to space
+// Reset when word is guessed
+// Don't count letters already guessed
+// What happens when someone wins
+
+
+// array of options 
