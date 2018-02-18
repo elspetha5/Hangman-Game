6b507ae6096cd59f9e04e88ea68be3e1f4d20ed9
Elspeth Anderson <elspetha@gmail.com>
2018-02-17 20:41:00 -0700

    almost completely different

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,87 @@
+//What variables do we need?
+var wins = document.getElementById("wins");
+var word = document.getElementById("word");
+var guessesLeft = document.getElementById("guessesLeft");
+var letters = document.getElementById("letters")
+
+var answer = ["cat", "doga", "tigera"];
+
+document.onkeyup = function (event) {
+    var alpha = event.key;
+    var userGuess = alpha.toLowerCase();
+    var wordText = word.innerHTML;
+    console.log(alpha);
+
+
+    //How do we start the game?
+    if (wordText.length == 0) {
+        //choose a random word
+        var idx = Math.floor(Math.random() * answer.length)
+        pickWord = answer[idx];
+        var wordElements = new Array(pickWord.length);
+        console.log(wordElements.length);
+        guessesLeft.innerHTML = 12;
+    } else if (wordText.length > 0) {
+        for (var i = 0; i < wordElements.length; i++) {
+            if (userGuess != wordElements[i]) {
+                var space = document.createElement("span");
+                space.innerHTML = "_ ";
+                word.appendChild(space);
+                guessesLeft--;
+            } else if (userGuess == wordElements[i]) {
+                var correct = document.createElement("span");
+                correct.innerHTML = userGuess;
+            }
+        }
+
+    }
+
+    /*} else if (wordText.length > 0) {
+        for (var i = 0; i < pickWord;)
+            pickWord[i].innerHTML = alpha;
+    } else if (alpha != pickWord[i]) {
+        var wrong = document.createElement("span");
+        wrong.innerHTML = alpha + ", ";
+        letters.appendChild(wrong);
+    }*/
+};
+
+
+
+//What happens when user presses a letter?
+    //recognize event
+
+    //store guessed letter in a variable
+    //What happens when user makes a wrong guess?
+        //put letter in wrong space
+        //make the guess count go down by one
+    //What happens when user makes a right guess?
+        //put letter in appropriate space in the word
+
+//What happens when user wins?
+    //screen gives congratulations message
+    //changes to a new word
+    //resets guesses
+    //clears wrong letters
+
+//What happens when user runs out of guesses?
+    //screen gives sadface
+    //shows answer
+    //changes to a new word
+    //resets guesses
+    //clears wrong letters
+
+
+
+
+
+
+
+
+
+
+
+/*
 //var wins = document.getElementById("#wins");
 var word = document.getElementById("word");
 //var guesses = document.getElementById("#guesses");
@@ -67,4 +151,4 @@ function lose() {
         space.removeChild(space.childNodes[0]);
         getNew.innerHTML = answer[0];
     }*/
-//};
+//};*/
